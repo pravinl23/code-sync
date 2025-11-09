80d58598e74ae66255a0eefdb72ae0fde85f7da7
Pravin Lohani <116219173+pravinl23@users.noreply.github.com>
2025-11-09 16:31:20 -0500

    Update solution for binary-tree-level-order-traversal

--- leetcode/binary-tree-level-order-traversal.py
+++ leetcode/binary-tree-level-order-traversal.py
@@ -1,24 +1,25 @@
 # Definition for a binary tree node.
-# class TreeNode(object):
+# class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
 #         self.val = val
 #         self.left = left
 #         self.right = right
-class Solution(object):
-    def levelOrder(self, root):
-        """
-        :type root: Optional[TreeNode]
-        :rtype: List[List[int]]
-        """
-        res = []
-        def dfs(node, counter):
+
+class Solution:
+    def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        buckets = [[]]
+
+        def recurse(node, height):
             if not node:
                 return
-            if counter == len(res):
-                res.append([])
-            res[counter].append(node.val)
-            dfs(node.left, counter + 1)
-            dfs(node.right, counter + 1)
-        
-        dfs(root, 0)
-        return res
+            if height > (len(buckets) - 1):
+                buckets.append([])
+
+            buckets[height].append(node.val)
+            recurse(node.left, height + 1)
+            recurse(node.right, height + 1)
+
+        recurse(root, 0)
+        return buckets
