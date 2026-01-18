b98bd240587919effe390d3ec0a92cd2ab7d2f63
Pravin Lohani <116219173+pravinl23@users.noreply.github.com>
2026-01-18 03:32:40 -0500

    Update solution for longest-consecutive-sequence

--- leetcode/longest-consecutive-sequence.py
+++ leetcode/longest-consecutive-sequence.py
@@ -1,25 +1,23 @@
-class Solution(object):
-    def longestConsecutive(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        max_len = 0
-        d = {}
-        for n in nums:
-            if n not in d:
-                d[n] = 1
-                left, right = 0,0
-                if n-1 in d:
-                    left = d[n-1]
-                if n+1 in d:
-                    right = d[n+1]
-                d[n] += left + right
-                d[n+right] = d[n]
-                d[n-left] = d[n]
-                
-                if d[n] > max_len:
-                    max_len = d[n]
+class Solution:
+    def longestConsecutive(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+            
+        nums_set = set(nums)
 
+        globalMax = 1
+        for i in range(len(nums)):
+            localMax = 0
+            next = nums[i]
+            prev = nums[i] - 1
+            while next in nums_set:
+                localMax += 1
+                nums_set.remove(next)
+                next += 1
+            while prev in nums_set:
+                localMax += 1
+                nums_set.remove(prev)
+                prev -= 1
+            globalMax = max(localMax, globalMax)
 
-        return max_len
+        return globalMax
