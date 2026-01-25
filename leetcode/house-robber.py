336656f311b9876508154d891b3eba09507b0d40
Pravin Lohani <116219173+pravinl23@users.noreply.github.com>
2026-01-25 00:45:24 -0500

    Update solution for house-robber

--- leetcode/house-robber.py
+++ leetcode/house-robber.py
@@ -4,22 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        if not nums:
-            return 0
-        dp = {}
         n = len(nums)
-        if n == 1:
-            return nums[0]
-        if n == 2:
-            return max(nums[0], nums[1])
+        if n <= 2:
+            return max(nums)
         if n == 3:
             return max(nums[0] + nums[2], nums[1])
 
+        dp = [0 for i in range(n)]
         dp[n - 1] = nums[n - 1]
-        dp[n - 2] = nums[n - 2]
-        dp[n - 3] = max(nums[n - 3] + nums[n - 1], nums[n - 2])
+        dp[n - 2] = max(nums[n - 2], nums[n - 1])
+        dp[n - 3] = max(nums[n - 3] + dp[n - 1], dp[n - 2])
         
         for i in range(n - 4, -1, -1):
-            dp[i] = max(nums[i] + dp[i + 2], dp[i + 1], nums[i] + dp[i + 3])
+            dp[i] = max(nums[i] + dp[i + 2], dp[i + 1])
 
         return dp[0]
